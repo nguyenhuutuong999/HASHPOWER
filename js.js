74efac1dc3d565897714f6f1a7eb4cce7792c714
nguyenhuutuong999@gmail.com <nguyenhuutuong999@gmail.com>
2019-10-15 21:05:21 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,23 @@
+function myFunction1(){
+    document.querySelector("#what-new").style.display = "block";
+    document.querySelector("#system-update").style.display = "none";
+    document.querySelector("#campaign").style.display = "none";
+}
+ function myFunction2(){
+   document.querySelector("#what-new").style.display = "none";
+    document.querySelector("#system-update").style.display = "block";
+    document.querySelector("#campaign").style.display = "none";
+}
+ function myFunction3(){
+    document.querySelector("#what-new").style.display = "none";
+    document.querySelector("#system-update").style.display = "none";
+    document.querySelector("#campaign").style.display = "block";
+}
+function myFunction4(){
+    document.querySelector("#box-extends").style.display = "none";
+    document.querySelector("#box").style.display = "flex";  
+}
+function myFunction5(){
+    document.querySelector("#box-extends").style.display = "block";
+    document.querySelector("#box").style.display = "none";  
+}
